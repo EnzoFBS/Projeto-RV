f0647953bfdf33a3d1d8a0903c41f8916a77727b
EnzoFBS <enzo.brigantesilvestre@gmail.com>
2024-06-14 23:31:16 -0300

    add mongoClient with connection mongodb

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,27 @@ import express from "express";
 import { config } from "dotenv";
 import { GetProteinsController } from "./controllers/get-proteins/get-proteins";
 import { DbMongoGetProteinsRepository } from "./repositories/get-proteins/mongo-get-proteins";
+import { MongoClient } from "./db/mongo";
 
-config();
+const main = async () => {
+  config();
 
-const app = express();
+  const app = express();
 
-const port = process.env.PORT || 8000;
+  await MongoClient.connect();
 
-app.get("/proteins", async (req, res) => {
-  const mongoGetProteinsRepository = new DbMongoGetProteinsRepository();
-  const getProteinsController = new GetProteinsController(
-    mongoGetProteinsRepository
-  );
+  app.get("/proteins", async (req, res) => {
+    const mongoGetProteinsRepository = new DbMongoGetProteinsRepository();
+    const getProteinsController = new GetProteinsController(
+      mongoGetProteinsRepository
+    );
 
-  const { body, statusCode } = await getProteinsController.handle();
-  res.send(body).status(statusCode);
-});
+    const { body, statusCode } = await getProteinsController.handle();
+    res.send(body).status(statusCode);
+  });
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+  const port = process.env.PORT || 8000;
+  app.listen(port, () => console.log(`listening on port ${port}`));
+};
+
+main();
